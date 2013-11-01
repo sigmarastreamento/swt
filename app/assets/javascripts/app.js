be19b278e07f9051fe21b63cec2758bf00d26b3f
Simão Menezes <simaomenezesneto@gmail.com>
2013-11-01 00:19:38 -0300

    Adicionado bootstrap ao projeto, adicionado mapa e angularjs.

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,4 @@
+// Declare app level module which depends on filters, and services
+angular.module('traccarApp', ['traccarApp.mapaControllers']).
+  config(['$routeProvider', function($routeProvider) {
+  }]);
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,6 +1,13 @@
 class Position < ActiveRecord::Base
-	self.table_name = 'positions'
-	self.primary_key = 'id'
+	self.table_name = "positions"
+	self.primary_key = "id"
 	
-	#fkp device_id
+	# metodo para não deixar o metodo valid do rails ser chamado
+	# devido a tabela positions ter um campo | atributo chamado valid
+	def self.instance_method_already_implemented?(method_name)
+    	return true if method_name == 'valid'
+    	return true if method_name == 'valid?'
+    	super
+	end
+
 end
