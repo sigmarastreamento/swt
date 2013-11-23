8a9748b9db951adf4c4127b137b5377b9836581b
Simão Menezes <simaomenezesneto@gmail.com>
2013-11-22 23:49:52 -0300

    criacao do metodo que faz a atualização dos devices.

--- app/assets/javascripts/js/MapViewModel.js
+++ app/assets/javascripts/js/MapViewModel.js
@@ -9,10 +9,11 @@ var MapViewModel = function(id, map) {
     
     self.loadDeviceByUser = function(jesusSuccess){
         $.ajax(SWT_SERVER.getDeviceByUser(function(data){
-            self.deviceShowMap(data);
-            /*for (var i = 0; i < data.length; i++){
-                console.log(data);
-            }*/
+            if(self.deviceLayer == undefined){
+                self.deviceShowMap(data);             
+            } else {
+                self.removeMarkerDevice(data);                
+            }
         }));
     };
     
@@ -28,14 +29,35 @@ var MapViewModel = function(id, map) {
     
     var marker_v;
     self.deviceShowMap = function(data){
-        var layer = self.getDeviceLayer();
-        
+        var layer = self.getDeviceLayer();        
         for (var i = 0; i < data.length; i++){
             var v = data[i];
             marker_v = UtilMap.createMarkerV(v, self.map);
             layer.addMarker(marker_v);
             self.map.panTo(marker_v.lonlat);
         }
+    };
+    
+  self.updateVehiclesMap = function() {
+        console.log("begin update device....");
+        self.loadDeviceByUser();
+        console.log("end update device....");
+    };
+    
+    self.removeMarkerDevice = function(data){
+        var layer = self.getDeviceLayer();
+        var markers = layer.markers;
+        for (i = 0; i < markers.length; i++){
+            layer.removeMarker(markers[i]);
+            i = --i;
+        }
+        console.log("marker removido");
+        self.deviceShowMap(data);
+    };
+    
+    
+    self.timerUpdateDeviceLonLat = function() {
+        setInterval(self.updateVehiclesMap, 15000);
     };
     
     
@@ -48,16 +70,17 @@ function init(map_id, viewmodel_id) {
 	map.addControl(new OpenLayers.Control.LayerSwitcher());    
 	var osm = new OpenLayers.Layer.OSM("CycleMap"); 
     var myLocation = UtilMap.convertToPoint({
-		longitude : -43.2228755950928,
-		latitude : -22.9853800175982
+		longitude : -38.521262200000024,
+		latitude : -3.7897738984758833
 	}, map);
 	map.addLayers([osm]);    
     map.setCenter(UtilMap.convertToLongLat({
-		longitude : -43.2228755950928,
-		latitude : -22.9853800175982
+		longitude : -38.521262200000024,
+		latitude : -3.7897738984758833
 	}, map), 12);
 	map.panTo(myLocation);    
 	mapaViewModel = new MapViewModel(viewmodel_id, map);
+    mapaViewModel.timerUpdateDeviceLonLat();
     mapaViewModel.loadDeviceByUser();
 }
 function initialize() {
